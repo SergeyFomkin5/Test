5db7b01359848b832ac14a16b5cd4895094376df
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-10-24 17:44:11 +0300

    Create character movement

---
+++ Assets/Player/Scripts/Player_CameraRotation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_CameraRotation : MonoBehaviour
+{
+    public float SensX;
+    public float SensY;
+
+    public Transform CameraAxis;
+
+    float RotationY;
+    float RotationX;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float MouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * SensX;
+        float MouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * SensY;
+
+        RotationY += MouseX;
+        RotationX -= MouseY;
+        RotationX = Mathf.Clamp(RotationX, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(RotationX, RotationY, 0);
+        CameraAxis.rotation = Quaternion.Euler(0, RotationY, 0);
+    }
+}
--- Assets/Player/Scripts/Player_movement.cs
+++ Assets/Player/Scripts/Player_movement.cs
@@ -4,48 +4,64 @@ using UnityEngine;
 
 public class Player_movement : MonoBehaviour
 {
-    public float speed = 6.0f;  // Скорость передвижения
-    public float mouseSensitivity = 2.0f;  // Чувствительность мыши
 
-    private CharacterController controller;
-    private Vector3 velocity;
+    [Header("Movement")]
+    public float moveSpeed;
 
-    private Transform cameraTransform;
-    private float xRotation = 0f;
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
 
-    void Start()
+    public Transform CameraAxis;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        controller = GetComponent<CharacterController>();
-        cameraTransform = Camera.main.transform;  // Получаем ссылку на камеру
-        Cursor.lockState = CursorLockMode.Locked;  // Захватываем курсор
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
     }
 
-    void Update()
+    private void Update()
     {
+        SpeedControl();
 
-        // Движение персонажа
-        float moveX = Input.GetAxis("Horizontal");  // Передвижение по оси X (A/D)
-        float moveZ = Input.GetAxis("Vertical");  // Передвижение по оси Z (W/S)
-
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        controller.Move(move * speed * Time.deltaTime);
-
-        // Вращение с помощью мыши
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-        // Поворот вокруг оси Y для вращения персонажа
-        transform.Rotate(Vector3.up * mouseX);
-
-        // Поворот камеры по оси X (ограничиваем поворот камеры)
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // Ограничиваем вращение по вертикали
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        MyInput();
     }
 
-    void LateUpdate()
+    private void FixedUpdate()
     {
-        // Привязываем камеру к позиции персонажа
-        cameraTransform.position = transform.position + new Vector3(0, 1.6f, 0);
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = CameraAxis.forward * verticalInput + CameraAxis.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 2f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
